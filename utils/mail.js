5577df712c0c0d72fce1b5715f2f6ab238a7b259
Ekomobong Archibong <ekomboy012@gmail.com>
2021-03-12 12:11:12 +0100

    **

--- utils/mail.js
+++ utils/mail.js
@@ -1,21 +1,44 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
+
+// // var transporter = nodemailer.createTransport({
+// //   service: 'gmail',
+// //   auth: {
+// //     user:process.env.user,
+// //     pass: process.env.pass
+// //   }
+// // });
 
 // var transporter = nodemailer.createTransport({
-//   service: 'gmail',
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   protocol: "tls",  
 //   auth: {
 //     user:process.env.user,
 //     pass: process.env.pass
 //   }
 // });
 
-var transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+// module.exports = transporter
+
+const nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+var transporter = nodemailer.createTransport(smtpTransport({
+  pool:true,
+  host: 'mail.foodnet.com.ng',
   port: 587,
-  protocol: "tls",  
+  //secure: true, // true for 465, false for other ports
   auth: {
-    user:process.env.user,
-    pass: process.env.pass
+    user: process.env.emailUser, // your domain email address
+    pass: process.env.emailPassword // your password
+  },
+  tls: { 
+  rejectUnauthorized: false 
   }
-});
+}));
 
 module.exports = transporter
