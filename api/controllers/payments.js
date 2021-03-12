c3fdf3a2491c2f047c02a3e0626a317e062f788f
Ekomobong Archibong <ekomboy012@gmail.com>
2021-03-12 12:22:16 +0100

    *

--- api/controllers/payments.js
+++ api/controllers/payments.js
@@ -13,7 +13,7 @@ const converter = require('number-to-words');
 
 
 exports.payments_verify_payment = (req,res,next) => { 
-    const {reference, email, mode_of_transport, weight_of_item, country_of_origin, shipment_cost, shipment_time} = req.body;
+    const {reference, customerEmail, adminEmail, mode_of_transport, weight_of_item, country_of_origin, shipment_cost, shipment_time} = req.body;
     
     const promise = paystack.verifyTransaction({
         reference
@@ -25,19 +25,34 @@ exports.payments_verify_payment = (req,res,next) => {
             // SEND EMAIL
             const mailOptions = {
                 from: `SuperFreighters <ekomboy012@gmail.com>`,
-                to: email,
+                to: customerEmail,
                 subject: "Order Details", 
-                html: `You have just placed a new order. <br /> Mode of Tranport: ${mode_of_transport} <br /> Weight of Tranport: ${weight_of_item}kg <br/> Country of Origin: ${country_of_origin} <br /> Shipment Cost: ${shipment_cost} <br /> Time of Delivery: Your order will arrive in Nigeria ${converter.toWords(shipment_time)} days after shipment.`
+                html: `Congrats! You have just placed a new order. <br /> Mode of Tranport: ${mode_of_transport} <br /> Weight of Tranport: ${weight_of_item}kg <br/> Country of Origin: ${country_of_origin} <br /> Shipment Cost: ${shipment_cost} <br /> Time of Delivery: Your order will arrive in Nigeria ${converter.toWords(shipment_time)} days after shipment.`
             } 
 
             transporter.sendMail(mailOptions, function(err,info){
                 if(err) {
                     // logger.debug('Error: ' + JSON.stringify(err));
                     console.log(err)
-                    return res.status(500).json({error:'An error occured while try to send mail. Please try again!'});
+                    return res.status(500).json({error:'An error occured while trying to send mail. Please try again!'});
                 } else {
 
-                        return res.status(200).json({message:"Your order has been successfully placed."})
+                    const mailOptions2 = {
+                        from: `SuperFreighters <ekomboy012@gmail.com>`,
+                        to: adminEmail,
+                        subject: "Order Details", 
+                        html: `Customer ${customerEmail} have just placed a new order. <br /> Mode of Tranport: ${mode_of_transport} <br /> Weight of Tranport: ${weight_of_item}kg <br/> Country of Origin: ${country_of_origin} <br /> Shipment Cost: ${shipment_cost} <br /> Time of Delivery: Your order will arrive in Nigeria ${converter.toWords(shipment_time)} days after shipment.`
+                    } 
+        
+                    transporter.sendMail(mailOptions2, function(err,info){
+                        if(err) {
+                    return res.status(500).json({error:'An error occured while trying to send mail. Please try again!'});
+
+                        } else {
+                    return res.status(200).json({message:"Your order has been successfully placed."})
+                        }   
+                        })
+
 
                     }
                 });
--- utils/mail.js
+++ utils/mail.js
@@ -1,22 +1,14 @@
 // const nodemailer = require('nodemailer');
 
-// // var transporter = nodemailer.createTransport({
-// //   service: 'gmail',
-// //   auth: {
-// //     user:process.env.user,
-// //     pass: process.env.pass
-// //   }
-// // });
-
 // var transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   protocol: "tls",  
+//   service: 'gmail',
 //   auth: {
 //     user:process.env.user,
 //     pass: process.env.pass
 //   }
 // });
+
+
 
 // module.exports = transporter
 
